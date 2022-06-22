e9b1a141f19cbd9b1c40e048a4528a9a2b086c9f
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-06-22 11:19:17 -0400

    Working example code

--- Realsense_Work/OpenGL_Test/PyOpenGL.py
+++ Realsense_Work/OpenGL_Test/PyOpenGL.py
@@ -6,11 +6,76 @@
 ## and then from there be able to localize a projection within our anybeam projector on to the wall
 ## localized to the realsense's reference frame
 
+import pygame
+from pygame.locals import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+verticies = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, -1, 1),
+    (-1, 1, 1)
+    )
+
+edges = (
+    (0,1),
+    (0,3),
+    (0,4),
+    (2,1),
+    (2,3),
+    (2,7),
+    (6,3),
+    (6,4),
+    (6,7),
+    (5,1),
+    (5,4),
+    (5,7)
+    )
+
+
+def Cube():
+    glBegin(GL_LINES)
+    for edge in edges:
+        for vertex in edge:
+            glVertex3fv(verticies[vertex])
+    glEnd()
+
+
+def main():
+    pygame.init()
+    display = (1370,720)
+    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+
+    glTranslatef(0.0,0.0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+
+main()
+
 #include <GL/glut.h>
 #include <math.h>
 #include <iostream>
 #include <librealsense2/rs.hpp>
-
+'''
 import PyOpenGL as gl
 
 def displayMe(void):
@@ -42,3 +107,4 @@ def main():
     gl.glutCreateWindow("Test 1: Display Triangle");
     gl.glutDisplayFunc(displayMe);
     gl.glutMainLoop();
+'''
