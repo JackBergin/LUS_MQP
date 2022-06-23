2f4be7db207eee8fb97664116bb804d0408e1df1
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-06-22 20:42:25 -0400

    Working Aruco Detection

--- Realsense_Work/ArUCo_Detection/src/ArUCo_Marker_Detection.py
+++ Realsense_Work/ArUCo_Detection/src/ArUCo_Marker_Detection.py
@@ -1,93 +1,77 @@
 #! /usr/bin/env python3
 
 # import the necessary packages
-import argparse
-import imutils
-import cv2
-import sys
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--home/parallels/Desktop/GitHub_Work/LUS_MQP/Realsense_Work/ArUCo_Detection/image/ArUCo Markers.png", required=True,
-	help="path to input image containing ArUCo tag")
-ap.add_argument("-t", "--type", type=str,
-	default="DICT_ARUCO_ORIGINAL",
-	help="type of ArUCo tag to detect")
-args = vars(ap.parse_args())
+import numpy as np
+import cv2, PIL, os
+from cv2 import aruco
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import pandas as pd
+ 
+filename = "ArucoTest_Color"
+frame = cv2.imread('Realsense_Work/ArUCo_Detection/image/'+filename+'.png')
+plt.figure()
+plt.imshow(frame)
+plt.show()
+ 
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
+parameters =  aruco.DetectorParameters_create()
+corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
+frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
 
-# define names of each possible ArUco tag OpenCV supports
-ARUCO_DICT = {
-	"DICT_4X4_50": cv2.aruco.DICT_4X4_50,
-	"DICT_4X4_100": cv2.aruco.DICT_4X4_100,
-	"DICT_4X4_250": cv2.aruco.DICT_4X4_250,
-	"DICT_4X4_1000": cv2.aruco.DICT_4X4_1000,
-	"DICT_5X5_50": cv2.aruco.DICT_5X5_50,
-	"DICT_5X5_100": cv2.aruco.DICT_5X5_100,
-	"DICT_5X5_250": cv2.aruco.DICT_5X5_250,
-	"DICT_5X5_1000": cv2.aruco.DICT_5X5_1000,
-	"DICT_6X6_50": cv2.aruco.DICT_6X6_50,
-	"DICT_6X6_100": cv2.aruco.DICT_6X6_100,
-	"DICT_6X6_250": cv2.aruco.DICT_6X6_250,
-	"DICT_6X6_1000": cv2.aruco.DICT_6X6_1000,
-	"DICT_7X7_50": cv2.aruco.DICT_7X7_50,
-	"DICT_7X7_100": cv2.aruco.DICT_7X7_100,
-	"DICT_7X7_250": cv2.aruco.DICT_7X7_250,
-	"DICT_7X7_1000": cv2.aruco.DICT_7X7_1000,
-	"DICT_ARUCO_ORIGINAL": cv2.aruco.DICT_ARUCO_ORIGINAL,
-	"DICT_APRILTAG_16h5": cv2.aruco.DICT_APRILTAG_16h5,
-	"DICT_APRILTAG_25h9": cv2.aruco.DICT_APRILTAG_25h9,
-	"DICT_APRILTAG_36h10": cv2.aruco.DICT_APRILTAG_36h10,
-	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
-}
-
-# load the input image from disk and resize it
-print("[INFO] loading image...")
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
-# verify that the supplied ArUCo tag exists and is supported by
-# OpenCV
-if ARUCO_DICT.get(args["type"], None) is None:
-	print("[INFO] ArUCo tag of '{}' is not supported".format(
-		args["type"]))
-	sys.exit(0)
-# load the ArUCo dictionary, grab the ArUCo parameters, and detect
-# the markers
-print("[INFO] detecting '{}' tags...".format(args["type"]))
-arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[args["type"]])
-arucoParams = cv2.aruco.DetectorParameters_create()
-(corners, ids, rejected) = cv2.aruco.detectMarkers(image, arucoDict,
-	parameters=arucoParams)
-
+print('Corners: ')
+print(corners)
+print(type(corners))
+ 
+p = []
+ 
 # verify *at least* one ArUco marker was detected
 if len(corners) > 0:
-	# flatten the ArUco IDs list
-	ids = ids.flatten()
-	# loop over the detected ArUCo corners
-	for (markerCorner, markerID) in zip(corners, ids):
-		# extract the marker corners (which are always returned in
-		# top-left, top-right, bottom-right, and bottom-left order)
-		corners = markerCorner.reshape((4, 2))
-		(topLeft, topRight, bottomRight, bottomLeft) = corners
-		# convert each of the (x, y)-coordinate pairs to integers
-		topRight = (int(topRight[0]), int(topRight[1]))
-		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-		topLeft = (int(topLeft[0]), int(topLeft[1]))
+ # flatten the ArUco IDs list
+ ids = ids.flatten()
+ # loop over the detected ArUCo corners
+ for (markerCorner, markerID) in zip(corners, ids):
+   # extract the marker corners (which are always returned in
+   # top-left, top-right, bottom-right, and bottom-left order)
+   corners = markerCorner.reshape((4, 2))
+   (topLeft, topRight, bottomRight, bottomLeft) = corners
+   # convert each of the (x, y)-coordinate pairs to integers
+   topRight = (int(topRight[0]), int(topRight[1]))
+   bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+   bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+   topLeft = (int(topLeft[0]), int(topLeft[1]))
+ 
+   # draw the bounding box of the ArUCo detection
+   cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+   cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+   cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+   cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+   # compute and draw the center (x, y)-coordinates of the ArUco
+   # marker
+   cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+   cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+   cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+ 
+   my_string = str((markerID, cX, cY))
+   my_list = my_string.split(",")
+   #my_string = str((markerID, cX, cY))
+   print(my_list)
+   p.append(my_list)
+   cv2.circle(frame, (cX, cY), 4, (0, 0, 255), -1)
+   # draw the ArUco marker ID on the frame
+   cv2.putText(frame, str(markerID),
+     (topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
+     0.5, (0, 255, 0), 2)
+   print("[INFO] ArUco marker ID: {}".format(markerID))
+   # show the output frame
+   cv2.imwrite('Realsense_Work/ArUCo_Detection/type' + str(filename) + '_det.png', frame)
+ plt.imshow(frame_markers)
+print(p)
+p = np.array(p)
+p = p.reshape((len(ids), 3))
+print(p.shape)
+print(p)
+np.save(str(filename) + '.npy', p)
 
-		# draw the bounding box of the ArUCo detection
-		cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
-		cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
-		cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
-		cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
-		# compute and draw the center (x, y)-coordinates of the ArUco
-		# marker
-		cX = int((topLeft[0] + bottomRight[0]) / 2.0)
-		cY = int((topLeft[1] + bottomRight[1]) / 2.0)
-		cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
-		# draw the ArUco marker ID on the image
-		cv2.putText(image, str(markerID),
-			(topLeft[0], topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-			0.5, (0, 255, 0), 2)
-		print("[INFO] ArUco marker ID: {}".format(markerID))
-		# show the output image
-		cv2.imshow("Image", image)
-		cv2.waitKey(0) 
