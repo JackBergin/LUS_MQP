b52e5d8f072f6637d6cbac90e8bfda9cbb4c7edc
Jack Bergin <81708456+JackBergin@users.noreply.github.com>
2022-08-28 18:44:58 -0400

    Working Segmentation

--- Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/color_mask_4_torso.py
+++ Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/color_mask_4_torso.py
@@ -1,25 +1,51 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Filename: color-filter.py
 
 import numpy as np
 import cv2
-
+import csv
+import matplotlib.pyplot as plt
 
 frame = cv2.imread('Object_Detection_Algorithims/Detectron_2_Processing/results/densepose_segment.0001.png')
 
 hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    #Blue Color
+#Blue Color
 blue_light=np.array([99,115,150],np.uint8)
 blue_dark=np.array([120,255,255],np.uint8)
 
+#Creates mask based off of the blue range
 mask = cv2.inRange(hsv, blue_light, blue_dark)
+#Illustrates the mask being applied
 res = cv2.bitwise_and(frame, frame, mask=mask)
+#Takes the greyscale image and creates non-zero binaries around it
+img = mask.astype(np.uint8)
+
+#Gets all non zero values
+coord = cv2.findNonZero(img)
+
+x = []
+y= []
+print(coord[0])
+for i in range(len(coord)):
+    #Isolates to [[x,y]]
+    parser = coord[i]
+    #Isolates to [x,y]
+    parser2 = parser[0]
+    #Isolates to x,y
+    x.append(parser2[0])
+    y.append(parser2[1])
+
+
+plt.figure(1)
+plt.plot(x, y, 'o', color='black');
+plt.xlabel('X', fontsize = 10)
+plt.ylabel('Y', fontsize = 10)
+ax = plt.gca()
+ax.invert_yaxis()
+plt.title('Body Segmentation Visualization', fontsize = 20)
+plt.show()
 
 cv2.imwrite('Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/results/frame.png', frame)
 cv2.imwrite('Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/results/hsv.png', hsv)
 cv2.imwrite('Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/results/mask.png', mask)
 cv2.imwrite('Object_Detection_Algorithims/Detectron_2_Processing/src/Current Methods/CV2/results/res.png', res)
-
-#cv2.destroyAllWindows()
